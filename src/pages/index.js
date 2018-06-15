a4cc918b2d18514c48a424d35d43a97566e568c4
Steven Natera <snatera09@gmail.com>
2018-06-15 19:46:08 -0400

    [feat] display the starters with the final styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,78 @@
 import 'semantic-ui-css/semantic.min.css';
 
 import React from 'react'
-import Link from 'gatsby-link'
 import styled from "styled-components";
-import { Grid, Card, Container, Image, Header, Button } from 'semantic-ui-react'
+import {
+  Grid,
+  Icon,
+  Card,
+  Container,
+  Image,
+  Header,
+  Button,
+  Menu
+} from 'semantic-ui-react'
 
-import matt from '../images/matthew.png'
 import { media } from "../components/media";
 import { SubscribeForm } from "../components/SubscribeForm";
+import { starters } from "../data/starters";
+
+
+const styledJumbotron = styled(Container)`
+  margin: 5rem auto auto auto;
+`;
+
+const styledJumbotronFooter = styled(Container)`
+  margin: 5rem auto 8rem auto;
+`;
+
+const styledHeader = styled(Header)`
+  &&& {
+    font-size: 3.5rem;
+  }
+`;
+
+const StyledSubheader = styled.p`
+  &&& {
+    font-size: 1.5rem;
+  }
+`;
+
+const StyledSubscribeButton = styled.button`
+  &&& {
+    cursor: pointer;
+    font-weight: bold;
+    margin-top: 1rem;
+    margin-left: 0.7rem;
+  }
+`
+
+const StyledCard = styled.div`
+  &&& {
+    margin: 2rem auto auto auto;
+  }
+`
+
+const StyledIcon = styled.i`
+  &&& {
+    margin-top: 20px;
+    font-size: 2rem;
+  }
+`
 
 class IndexPage extends React.Component {
 
   state = {
+    links: starters,
+  }
 
-    links: [
-      'hello/world',
-      'world/hellow',
-      'appels/meals',
-      'owner/repo',
-    ]
+  handleClick = url => {
+    // send event to google analytics
+
+    window.open(url)
   }
 
   render() {
-
-    const styledJumbotron = styled(Container)`
-      margin: 60px auto;
-
-      ${media.small`
-      `};
-
-      ${media.medium`
-      `};
-    `;
-
-    const styledHeader = styled(Header)`
-      &&& {
-        font-size: 3.5rem;
-      }
-    `;
-
-    const StyledSubheader = styled.p`
-      &&& {
-        font-size: 1.5rem;
-      }
-    `;
-
-    const StyledSubscribeButton = styled.button`
-      &&& {
-        cursor: pointer;
-        font-weight: bold;
-        margin-top: 1rem;
-        margin-left: 0.7rem;
-
-        ${media.small`
-
-        `};
-
-        ${media.medium`
-        `};
-      }
-    `
-
 
     // Mailchimp endpoint
     // From: https://us17.admin.mailchimp.com/lists/integration/embeddedcode?id=XXXXXX
@@ -70,52 +82,85 @@ class IndexPage extends React.Component {
     const MAILCHIMP_URL = `https://gatsbymanor.us17.list-manage.com/subscribe/post-json?u=6d5879814f1b3ecd3667f0c47&amp;id=a66cece897`
 
     return (
-      <Container textAlign='center'>
-        <Container as={styledJumbotron}>
-          <Header as={styledHeader}>Gatsby Manor</Header>
-          <StyledSubheader>
-            Gatsby Starters for all types of projects
-          </StyledSubheader>
+      <div>
+        <Menu secondary>
+
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <SubscribeForm
+                mailchimp_url={MAILCHIMP_URL}>
+                {(submit) => (
+                  <Button type="submit" as={StyledSubscribeButton} color='blue'
+                    onClick={(e) => submit(e)}>
+                      Join our mailing list!
+                  </Button>
+                )}
+              </SubscribeForm>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+
+        <Container textAlign='center'>
+
+          <Container as={styledJumbotron}>
+            <Header as={styledHeader}>Gatsby Manor</Header>
+            <StyledSubheader>
+              Gatsby Starters for all types of projects
+            </StyledSubheader>
+
+            <Container>
+              <Icon onClick={() => this.handleClick(`https://twitter.com/thegatsbymanor`)} as={StyledIcon} name="twitter" />
+              <Icon onClick={() => this.handleClick(`https://github.com/gatsbymanor`)} as={StyledIcon} name="github" />
+              <Icon onClick={() => this.handleClick(`http://www.medium.com`)} as={StyledIcon} name="mail" />
+            </Container>
+          </Container>
+
+          <Grid centered columns={1}>
+            <Grid.Row>
+              {this.state.links.map((obj, idx) => {
+
+                return (
+                  <Grid.Column key={idx}>
+                    <Card key={idx} as={StyledCard}>
+                      <Image src={obj.image} />
+                      <Card.Content>
+                        <Card.Header>{obj.name}</Card.Header>
+                        <Card.Description>{obj.perks}</Card.Description>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <a onClick={() => this.handleClick(obj.source)}>
+                          <Button basic color='blue' fluid>Download</Button>
+                        </a>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <a onClick={() => this.handleClick(obj.demo)}>
+                          <Button basic color='green' fluid>Demo</Button>
+                        </a>
+                      </Card.Content>
+                    </Card>
+                  </Grid.Column>
+                )
+              })}
+            </Grid.Row>
+          </Grid>
+
+          <Container as={styledJumbotronFooter}>
+            <Header as={styledHeader}>Join our newsletter!</Header>
+            <StyledSubheader>
+              Subscribe for news and updates about Starters
+            </StyledSubheader>
+            <SubscribeForm
+              mailchimp_url={MAILCHIMP_URL}>
+              {(submit) => (
+                <Button type="submit" as={StyledSubscribeButton} color='blue'
+                  onClick={(e) => submit(e)}>
+                    Subscribe
+                </Button>
+              )}
+            </SubscribeForm>
+          </Container>
         </Container>
-
-        <Grid centered columns={4}>
-          <Grid.Row>
-            {this.state.links.map((obj, idx) => {
-
-              return (
-                <Grid.Column key={idx}>
-                  <Card key={idx} style={{}}>
-                    <Image src={matt} />
-                    <Card.Content>
-                      <Card.Header>{obj}</Card.Header>
-                      <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                      <Button basic color='blue' fluid>Download</Button>
-                    </Card.Content>
-                  </Card>
-                </Grid.Column>
-              )
-            })}
-          </Grid.Row>
-        </Grid>
-
-        <Container as={styledJumbotron}>
-          <Header as={styledHeader}>Join our newsletter!</Header>
-          <StyledSubheader>
-            Subscribe for news and updates about Starters
-          </StyledSubheader>
-          <SubscribeForm
-            mailchimp_url={MAILCHIMP_URL}>
-            {(submit) => (
-              <Button type="submit" as={StyledSubscribeButton} color='blue'
-                onClick={(e) => submit(e)}>
-                  Subscribe
-              </Button>
-            )}
-          </SubscribeForm>
-        </Container>
-      </Container>
+      </div>
     )
   }
 
